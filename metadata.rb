b74cf47dc229ec250cf63bd18199bd60ea3ffe47
Greg Hellings <greg@thesub.net>
2014-11-24 12:41:37 -0800

    Fixed missing dependencies in metadata file and added supports distro

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,13 @@ maintainer_email 'greg@thesub.net'
 license          'Apache V2'
 description      'Installs/Configures fog_gem'
 long_description 'Installs/Configures fog_gem'
-version          '0.1.0'
+version          '0.1.1'
 
 depends          'nokogiri'
+depends          'apt'
+depends          'yum'
+
+%w{ amazon redhat ubuntu centos}.each do |distro|
+	supports distro
+end
 
